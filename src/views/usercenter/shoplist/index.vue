745ee2bb285b13b86b822501c0ebadf6b80c0c02
zhuwencheng <zhuwencheng>
2018-06-01 18:01:04 +0800

    '1'

--- src/views/usercenter/shoplist/index.vue
+++ src/views/usercenter/shoplist/index.vue
@@ -5,56 +5,139 @@
 <template>
     <div class="shoplist">
         <div class="leftmenu">
-            <dl>
-                <dt class="active">果茶系列<em></em></dt>
-                <dd>热饮系列</dd>
-                <dd>热饮系列</dd>
-                <dd>热饮系列</dd>
-            </dl>
-            <dl>
-                <dt>果茶系列</dt>
-                <dd>热饮系列</dd>
-                <dd>热饮系列</dd>
-                <dd>热饮系列</dd>
-            </dl>
-            <dl>
-                <dt>果茶系列</dt>
-                <dd>热饮系列</dd>
-                <dd>热饮系列</dd>
-                <dd>热饮系列</dd>
+            <dl v-for="(item,i) in menuList" :key="i"  :class="{active:item.checked}">
+                <dt @click="changeMenu(item)">{{item.title}}<em></em></dt>
+                <dd v-for="(litem,index) in item.children" :key="index+'asdb'">{{litem.title}}</dd>
             </dl>
         </div>
-        <div class="ct"></div>
+        <div class="ct">
+            <div class="shopcart"></div>
+            <div class="search">
+                <input type="text" placeholder="搜索" />
+            </div>
+            <div class="bd">
+                 <Scroll :on-reach-bottom="handleReachBottom" :distance-to-edge='distance'>
+                    <dl class="card">
+                        <dt><img src="http://iph.href.lu/270x270" /></dt>
+                        <dd>招牌奶茶</dd>
+                        <dd class="price">￥20.00</dd>
+                    </dl>
+                    <dl class="card">
+                        <dt><img src="http://iph.href.lu/270x270" /></dt>
+                        <dd>招牌奶茶</dd>
+                        <dd class="price">￥20.00</dd>
+                    </dl>
+                    <dl class="card">
+                        <dt><img src="http://iph.href.lu/270x270" /></dt>
+                        <dd>招牌奶茶</dd>
+                        <dd class="price">￥20.00</dd>
+                    </dl>
+                    <dl class="card">
+                        <dt><img src="http://iph.href.lu/270x270" /></dt>
+                        <dd>招牌奶茶</dd>
+                        <dd class="price">￥20.00</dd>
+                    </dl>
+                    <dl class="card">
+                        <dt><img src="http://iph.href.lu/270x270" /></dt>
+                        <dd>招牌奶茶</dd>
+                        <dd class="price">￥20.00</dd>
+                    </dl>
+                    <dl class="card">
+                        <dt><img src="http://iph.href.lu/270x270" /></dt>
+                        <dd>招牌奶茶</dd>
+                        <dd class="price">￥20.00</dd>
+                    </dl>
+                    <dl class="card">
+                        <dt><img src="http://iph.href.lu/270x270" /></dt>
+                        <dd>招牌奶茶</dd>
+                        <dd class="price">￥20.00</dd>
+                    </dl>
+                    <dl class="card">
+                        <dt><img src="http://iph.href.lu/270x270" /></dt>
+                        <dd>招牌奶茶</dd>
+                        <dd class="price">￥20.00</dd>
+                    </dl>
+                 </Scroll>
+            </div>
+        </div>
     </div>
     
 </template>
 
 <script>
 export default {
-  components: {
-   
-  },
+  components: {},
   data() {
     return {
-      menuList:[
-          {
-              title:'果茶系列',
-              children:[
-                  {
-                      title:'子类',
-                      id:'001'
-                  }
-              ]
-          }
+      distance:-100,
+      menuList: [
+        {
+          title: "果茶系列",
+          children: [
+            {
+              title: "子类1",
+              id: "001"
+            },
+            {
+              title: "子类2",
+              id: "001"
+            },
+            {
+              title: "子类3",
+              id: "001"
+            }
+          ]
+        },
+        {
+          title: "果茶系列2",
+          children: [
+            {
+              title: "子类1",
+              id: "001"
+            },
+            {
+              title: "子类2",
+              id: "001"
+            },
+            {
+              title: "子类3",
+              id: "001"
+            }
+          ]
+        },
+        {
+          title: "果茶系列3",
+          children: [
+            {
+              title: "子类1",
+              id: "001"
+            },
+            {
+              title: "子类2",
+              id: "001"
+            },
+            {
+              title: "子类3",
+              id: "001"
+            }
+          ]
+        }
       ]
     };
   },
   methods: {
-    
+    changeMenu(item) {
+      if (item.checked) {
+        item.checked = !item.checked;
+      } else {
+        this.$set(item, "checked", true);
+      }
+    },
+    handleReachBottom(){
+        console.log(111)
+    }
   },
-  mounted() {
-    
-  }
+  mounted() {}
 };
 </script>
 
